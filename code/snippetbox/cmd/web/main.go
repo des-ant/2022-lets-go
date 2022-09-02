d39d3b372f6cbac667667ece0eae25abd0bff180
des-ant <57375593+des-ant@users.noreply.github.com>
2022-09-02 23:48:33 +1000

    Add template cache

--- code/snippetbox/cmd/web/main.go
+++ code/snippetbox/cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"flag"
+	"html/template" // New import
 	"log"
 	"net/http"
 	"os"
@@ -17,12 +18,12 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// Add a snippets field to the application struct. This will allow us to
-// make the SnippetModel object available to our handlers.
+// Add a templateCache field to the application struct.
 type application struct {
-	errorLog *log.Logger
-	infoLog  *log.Logger
-	snippets *models.SnippetModel
+	errorLog      *log.Logger
+	infoLog       *log.Logger
+	snippets      *models.SnippetModel
+	templateCache map[string]*template.Template
 }
 
 func main() {
@@ -40,12 +41,18 @@ func main() {
 	}
 	defer db.Close()
 
-	// Initialize a models.SnippetModel instance and add it to the application
-	// dependencies.
+	// Initialize a new template cache...
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	// And add it to the application dependencies.
 	app := &application{
-		errorLog: errorLog,
-		infoLog:  infoLog,
-		snippets: &models.SnippetModel{DB: db},
+		errorLog:      errorLog,
+		infoLog:       infoLog,
+		snippets:      &models.SnippetModel{DB: db},
+		templateCache: templateCache,
 	}
 
 	srv := &http.Server{
