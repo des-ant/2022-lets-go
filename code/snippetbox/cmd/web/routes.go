5fc1eeeb38a50d9494f3e04c7621f0aaf159a055
des-ant <57375593+des-ant@users.noreply.github.com>
2022-09-16 22:03:19 +1000

    Set up custom handler to deal with 404 responses

--- code/snippetbox/cmd/web/routes.go
+++ code/snippetbox/cmd/web/routes.go
@@ -3,28 +3,29 @@ package main
 import (
 	"net/http"
 
-	"github.com/julienschmidt/httprouter" // New import
+	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
-	// Initialize the router.
 	router := httprouter.New()
 
-	// Update the pattern for the route for the static files.
+	// Create a handler function which wraps our notFound() helper, and then
+	// assign it as the custom handler for 404 Not Found responses. You can also
+	// set a custom handler for 405 Method Not Allowed responses by setting
+	// router.MethodNotAllowed in the same way too.
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	// And then create the routes using the appropriate methods, patterns and
-	// handlers.
 	router.HandlerFunc(http.MethodGet, "/", app.home)
 	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
 	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
 	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
-	// Create the middleware chain as normal.
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-
-	// Wrap the router with the middleware and return it as normal.
 	return standard.Then(router)
 }
