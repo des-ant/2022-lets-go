a9cd863c32a7271903f086306cb963a242306ba3
des-ant <57375593+des-ant@users.noreply.github.com>
2022-11-18 19:06:40 +1100

    Add interface for snippetModel and userModel

--- code/snippetbox/internal/models/snippets.go
+++ code/snippetbox/internal/models/snippets.go
@@ -2,9 +2,15 @@ package models
 
 import (
 	"database/sql"
-	"errors" // New import
+	"errors"
 	"time"
 )
+
+type SnippetModelInterface interface {
+	Insert(title string, content string, expires int) (int, error)
+	Get(id int) (*Snippet, error)
+	Latest() ([]*Snippet, error)
+}
 
 // Define a Snippet type to hold the data for an individual snippet. Notice how
 // the fields of the struct correspond to the fields in our MySQL snippets
